02b824b4db4c9502f0397addd757cccbbf7dffbc
Ani0769 <w0769190@myscc.ca>
2024-09-18 10:46:59 -0400

    assignment 2

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,9 +1,9 @@
 export interface User {
-  title:' Perfumes and fragnances';
-  businessowner: 'Anil Kumar';
-  businessname: 'Royal Oud';
-  product: 'Perfumes' ;
-  about : 'This business is about fragnances and natural oud oil which produce non-alcoholic perfumes';
+  id:'number';
+  firstname: 'string';
+  lastname: 'string';
+  interest: 'string' ;
+  isAdmin? : 'boolean';
 
 
 }
